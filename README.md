e297ec408e611bd426571d26d52021b28b2e39ff
Suraj Pillai <85.suraj@gmail.com>
2020-02-19 10:50:31 -0500

    Format script and update readme

--- README.md
+++ README.md
@@ -13,13 +13,15 @@ Windows or linux machine with the following installations
 ## Instructions for running the script
 
 1) Copy the script to your source-tracked sfdx project folder
-2) Run script in a bash shell
-3) Go to the 'local' folder and review the files
+2) Create a folder named 'local' in your project root. Add it to the .gitignore file. If the directory exists, empty it before every run. Make sure there is an 'sfdx-project.json' file in the 'local' directory if you plan on running 'convert' or 'deploy' commands afterwards.
+2) Run script in a bash shell.
+3) Go to the 'local' folder and review the files. 
 4) Run *force:source:convert* to convert the files to mdapi format
 5) Run *force:mdapi:deploy* to deploy the files to your org
 
 
 ## Notes
 
-- ## Please review the generated files thoroughly before deploying. This script is not fool-proof by any means.
--  Script does not generate deployable artifacts for deletes
+- ## Please review the generated files thoroughly before deploying. This script is not fool-proof by any means. It has not been tested with all possible metadata components
+- Script does not generate deployable artifacts for deletes
+- If you get an 'invalid character' error, run 'dos2unix' command on the file.
--- diffpackager.sh
+++ diffpackager.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 OIFS="$IFS"
 IFS=$'\n'
+if [ $# -lt 1 ]
+then
+ echo 'Usage: ./diffpackager.sh <your commit hash to diff from> <your commit has to diff to>(optional, if not specified, defaults to "HEAD")'
+ exit 1;
+fi
 # Salesforce deletes aura component files that were not included in the deployment. So, you have to ensure that you deploy all the files even if only 1 file in a component is changed. Eg: Even if you change just the .cmp file, you still have to deploy the controller, helper, css files, etc.
-# Replace f7815723e99 with your commit id
-for var in `git diff --diff-filter=ACMRT --name-only f7815723e99 HEAD`;do if [[ "$var" = *"/aura/"* ]];then tar -rf local/myfiles.tar `dirname "$var"`;else tar -rf local/myfiles.tar "$var";if [ -f "$var-meta.xml" ];then  tar -rf local/myfiles.tar "$var-meta.xml";fi;fi;done
+# script assumes you have a 'local' directory in the project root with an sfdx-project.json file in it. If not, create one
+# if you get an 'invalid character' error, run 'dos2unix' command on the file
+# delete the 'local/force-app' and 'local/myfiles.tar' before every new run
+toHash=${2:-HEAD}
+for var in `git diff --diff-filter=ACMRT --name-only $1 $toHash` # loop over all new and modified files in git diff
+do
+	if [[ "$var" = *"/aura/"* ]] || [[ "$var" = *"/lwc/"* ]] # for aura and lwc, grab the whole directory and not just the changed file
+	then
+		tar -rf local/myfiles.tar `dirname "$var"` # add files to a 'tar' archive. we will expand it to recreate the directory structure after we are done
+	else tar -rf local/myfiles.tar "$var"
+		if [ -f "$var-meta.xml" ] # if there is a meta.xml file, grab that as well
+		then
+			tar -rf local/myfiles.tar "$var-meta.xml"
+		fi
+	fi
+done
 cd local
-tar -xvf myfiles.tar
+tar -xf myfiles.tar # expand tar file to recreate the directory structure, relative to the project root
 #Uncomment these lines if you wish to package the source and deploy to an org 
-#cd local
 #sfdx force:source:convert -r .\force-app\ -d src -p mypackagename
 #sfdx force:mdapi:deploy -c -d src -u myorg -w 20
